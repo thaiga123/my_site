134b6ecb8b0b7b51d283b4a37bdd5c86ec84a880
thaiga123 <ajmeyer915@gmail.com>
2024-12-13 10:48:58 +0200

    ADDED RESUME

--- website.py
+++ website.py
@@ -10,5 +10,9 @@ def homepage():
 def about():
     return render_template("about.html")
 
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
